d55b8e55fd3187dbce2992cd46843f99439c904f
Mehdi K <khajoo@gmail.com>
2017-05-06 16:25:50 +0100

    Fix migration issue by adding an empty migration!!

---
+++ Model/Migrations/201705061424528_Fix_migration_issues.cs
@@ -0,0 +1,16 @@
+namespace LightMethods.Survey.Models.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+
+    public partial class Fix_migration_issues : DbMigration
+    {
+        public override void Up()
+        {
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
