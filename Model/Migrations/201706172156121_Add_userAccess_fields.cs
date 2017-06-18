e1e6a9da079ca93d02d5e0cb368e4fdbe26097bd
Armin Zia <arminzia@live.com>
2017-06-18 20:37:03 +0430

    added default values to migration script

--- Model/Migrations/201706172156121_Add_userAccess_fields.cs
+++ Model/Migrations/201706172156121_Add_userAccess_fields.cs
@@ -2,15 +2,15 @@ namespace LightMethods.Survey.Models.Migrations
 {
     using System;
     using System.Data.Entity.Migrations;
-    
+
     public partial class Add_userAccess_fields : DbMigration
     {
         public override void Up()
         {
-            AddColumn("dbo.AspNetUsers", "IsWebUser", c => c.Boolean());
-            AddColumn("dbo.AspNetUsers", "IsMobileUser", c => c.Boolean());
+            AddColumn("dbo.AspNetUsers", "IsWebUser", c => c.Boolean(defaultValue: true));
+            AddColumn("dbo.AspNetUsers", "IsMobileUser", c => c.Boolean(defaultValue: true));
         }
-        
+
         public override void Down()
         {
             DropColumn("dbo.AspNetUsers", "IsMobileUser");
