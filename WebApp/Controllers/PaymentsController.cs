91c4c993acd51511ee5f685f60e9be0250319fe7
Armin Zia <arminzia@live.com>
2017-06-24 02:53:58 +0430

    added payments and subscriptions controllers

---
+++ WebApp/Controllers/PaymentsController.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using LightMethods.Survey.Models.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class PaymentsController : BaseApiController
+    {
+        PaymentsRepository Payments { get { return UnitOfWork.PaymentsRepository; } }
+
+        [Route("api/payments/{userId:guid}")]
+        [ResponseType(typeof(IEnumerable<PaymentRecordDTO>))]
+        public IHttpActionResult Get(Guid userId)
+        {
+            var payments = this.Payments.AllAsNoTracking
+                .Where(p => p.OrgUserId == userId)
+                .ToList();
+            var result = payments.Select(p => Mapper.Map<PaymentRecordDTO>(p));
+
+            return Ok(result);
+        }
+    }
+}
---
+++ WebApp/Controllers/SubscriptionsController.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using LightMethods.Survey.Models.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class SubscriptionsController : BaseApiController
+    {
+        SubscriptionsRepository Subscriptions { get { return UnitOfWork.SubscriptionsRepository; } }
+
+        [Route("api/subscriptions/{userId:guid}")]
+        [ResponseType(typeof(IEnumerable<SubscriptionDTO>))]
+        public IHttpActionResult Get(Guid userId)
+        {
+            var subscriptions = this.Subscriptions.AllAsNoTracking
+                .Where(s => s.OrgUserId == userId)
+                .ToList();
+            var result = subscriptions.Select(s => Mapper.Map<SubscriptionDTO>(s));
+
+            return Ok(result);
+        }
+
+        [Route("api/subscriptions/getExpiry/{userId:guid}")]
+        [ResponseType(typeof(SubscriptionExpiryDTO))]
+        public IHttpActionResult GetExpiry(Guid userId)
+        {
+            var result = new SubscriptionExpiryDTO();
+            var subscriptions = this.Subscriptions.AllAsNoTracking
+                .Where(s => s.OrgUserId == userId)
+                .ToList();
+
+            var lastSubscription = subscriptions.LastOrDefault();
+            if (lastSubscription != null)
+                result.ExpiryDate = lastSubscription.EndDate;
+
+            return Ok(result);
+        }
+    }
+}
