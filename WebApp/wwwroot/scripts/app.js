b5374f061f32d41ce0bda701857a1152be440103
Armin Zia <arminzia@live.com>
2017-06-25 13:47:24 +0430

    add angular-currency-format

--- WebApp/wwwroot/scripts/app.js
+++ WebApp/wwwroot/scripts/app.js
@@ -30,6 +30,7 @@ var App;
         "chart.js",
         "uiGmapgoogle-maps",
         "minicolors",
-        "ngTagsInput"
+        "ngTagsInput",
+        "currencyFormat"
     ]);
 })(App || (App = {}));
