d45b8e14577392978f4c2610b5fc2cb8b871ac15
Armin Zia <arminzia@live.com>
2017-07-18 19:58:22 +0430

    fixed HasTimeValue migration script

--- Model/Migrations/201707131856366_Add_hasTimeValue_to_date_metric.cs
+++ Model/Migrations/201707131856366_Add_hasTimeValue_to_date_metric.cs
@@ -2,14 +2,15 @@ namespace LightMethods.Survey.Models.Migrations
 {
     using System;
     using System.Data.Entity.Migrations;
-    
+
     public partial class Add_hasTimeValue_to_date_metric : DbMigration
     {
         public override void Up()
         {
             AddColumn("dbo.Metrics", "HasTimeValue", c => c.Boolean());
+            Sql("UPDATE dbo.Metrics SET HasTimeValue = 0");
         }
-        
+
         public override void Down()
         {
             DropColumn("dbo.Metrics", "HasTimeValue");
