44ea02dad36d4b5d6119ee6b9ddcf21d36a1f5f2
Armin Zia <arminzia@live.com>
2017-07-20 17:34:53 +0430

    add DateTimeService for wrapping Now values

---
+++ Model/Services/DateTimeService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LightMethods.Survey.Models.Services
+{
+    public static class DateTimeService
+    {
+        public static DateTime Now
+        {
+            get { return DateTime.Now; }
+        }
+
+        public static DateTime UtcNow
+        {
+            get { return DateTime.UtcNow; }
+        }
+    }
+}
