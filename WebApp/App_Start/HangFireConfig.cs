d588bb4c0d55af97b874b472f5631844569d89d0
Armin Zia <arminzia@live.com>
2017-08-24 18:57:30 +0430

    add email service and recurring job. removed GET/DEL actions from feedbacks.

--- WebApp/App_Start/HangFireConfig.cs
+++ WebApp/App_Start/HangFireConfig.cs
@@ -1,9 +1,5 @@
 ﻿using Hangfire;
 using Owin;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace WebApi
 {
@@ -15,6 +11,9 @@ namespace WebApi
 
             app.UseHangfireDashboard();
             app.UseHangfireServer();
+
+            var emailService = new WebApi.Services.EmailService();
+            RecurringJob.AddOrUpdate("process-emails", () => emailService.ProcessEmails(), Cron.MinuteInterval(1));
         }
     }
 }
---
+++ WebApp/Services/EmailService.cs
@@ -0,0 +1,56 @@
+﻿using Hangfire;
+using LightMethods.Survey.Models.DAL;
+using LightMethods.Survey.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace WebApi.Services
+{
+    public class EmailService
+    {
+        private UnitOfWork UnitOfWork { get; set; }
+
+        public EmailService()
+        {
+            this.UnitOfWork = new UnitOfWork(new SurveyContext(autoDetectChangesEnabled: false));
+        }
+
+        public void ProcessEmails()
+        {
+            var emails = this.UnitOfWork.EmailsRepository.AllAsNoTracking
+                .Where(e => !e.IsSent)
+                .OrderByDescending(e => e.DateCreated)
+                .ToList();
+
+            foreach (var email in emails)
+                BackgroundJob.Enqueue(() => this.SendEmail(email));
+        }
+
+        public async Task SendEmail(Email email)
+        {
+            var message = new MailMessage();
+            message.To.Add(email.To);
+            message.Subject = email.Subject;
+            message.Body = email.Content;
+            message.IsBodyHtml = true;
+
+            try
+            {
+                var client = new SmtpClient();
+                await client.SendMailAsync(message);
+
+                email.IsSent = true;
+                this.UnitOfWork.EmailsRepository.InsertOrUpdate(email);
+                this.UnitOfWork.Save();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
