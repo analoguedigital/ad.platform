a50fc806ac277b756ed99008322a54925a70744c
Armin Zia <arminzia@live.com>
2017-09-24 19:00:47 +0330

    added gulp tasks for a release build

--- WebApp/gulpfile.js
+++ WebApp/gulpfile.js
@@ -1,9 +1,16 @@
 ﻿'use strict';
 
-var gulp = require('gulp'),
-    debug = require('gulp-debug'),
-    Config = require('./gulpfile.config'),
-    sass = require('gulp-sass');
+var gulp = require('gulp');
+var domSrc = require('gulp-dom-src');
+var concat = require('gulp-concat');
+var cssmin = require('gulp-cssmin');
+var uglify = require('gulp-uglify');
+var cheerio = require('gulp-cheerio');
+var templateCache = require('gulp-angular-templatecache');
+
+var debug = require('gulp-debug');
+var Config = require('./gulpfile.config');
+var sass = require('gulp-sass');
 
 var config = new Config();
 
@@ -26,7 +33,56 @@ gulp.task('watch', function () {
     gulp.watch(config.allSassFiles, ['sass']);
 });
 
-
 gulp.task('default', [
     'sass',
     'watch']);
+
+
+gulp.task('templates', function () {
+    return gulp.src('wwwroot/comp/**/*.html')
+        .pipe(templateCache({
+            root: 'comp',
+            module: 'app',
+            filename: 'app.components-tpl.min.js'
+        }))
+        .pipe(gulp.dest('wwwroot/dist/'));
+});
+
+gulp.task('directives', function () {
+    return gulp.src('wwwroot/scripts/directives/**/*.html')
+        .pipe(templateCache({
+            root: 'scripts/directives',
+            module: 'app',
+            filename: 'app.directives-tpl.min.js'
+        }))
+        .pipe(gulp.dest('wwwroot/dist/'));
+});
+
+gulp.task('css', function () {
+    return domSrc({ file: 'wwwroot/index.html', selector: 'link', attribute: 'href' })
+        .pipe(concat('app.full.min.css'))
+        .pipe(cssmin())
+        .pipe(gulp.dest('wwwroot/dist/'));
+});
+
+gulp.task('js', function () {
+    return domSrc({ file: 'wwwroot/index.html', selector: 'script', attribute: 'src' })
+        .pipe(concat('app.full.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('wwwroot/dist/'));
+});
+
+gulp.task('indexHtml', function () {
+    return gulp.src('wwwroot/index.html')
+        .pipe(cheerio(function ($) {
+            $('script').remove();
+            $('link').remove();
+            $('body').append('<script src="app.full.min.js"></script>');
+            $('body').append('<script src="app.components-tpl.min.js"></script>');
+            $('body').append('<script src="app.directives-tpl.min.js"></script>');
+            $('head').append('<link rel="stylesheet" href="app.full.min.css">');
+        }))
+        .pipe(gulp.dest('wwwroot/dist/'));
+});
+
+gulp.task('build', ['css', 'templates', 'directives', 'js', 'indexHtml']);
