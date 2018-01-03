af17dbdd14e8bd3e8f41759887b81776fbc4bcd0
Armin Zia <arminzia@live.com>
2018-01-03 16:25:33 +0330

    updated reset password form. tokens and emails now come from query params.

--- WebApp/wwwroot/comp/setPassword/setPasswordController.ts
+++ WebApp/wwwroot/comp/setPassword/setPasswordController.ts
@@ -12,6 +12,8 @@ module App {
     interface ISetPasswordControllerScope extends ng.IScope {
         model: ISetPasswordModel;
         isMatch: boolean;
+        token: string;
+        emailAddress: string;
 
         checkMatch: () => void;
         resetPassword: (form: ng.IFormController) => void;
@@ -23,11 +25,12 @@ module App {
     }
 
     class SetPasswordController implements ISetPasswordController {
-        static $inject: string[] = ["$scope", "$resource", "$state", "toastr"];
+        static $inject: string[] = ["$scope", "$resource", "$state", "$stateParams", "toastr"];
         constructor(
             private $scope: ISetPasswordControllerScope,
             private $resource: ng.resource.IResourceService,
             private $state: ng.ui.IStateService,
+            private $stateParams: ng.ui.IStateParamsService,
             private toastr: any) {
             this.activate();
         }
@@ -38,6 +41,18 @@ module App {
 
             this.$scope.checkMatch = () => { this.checkMatch(); }
             this.$scope.resetPassword = (form: ng.IFormController) => { this.resetPassword(form); }
+
+            var token = this.$stateParams.token;
+            var email = this.$stateParams.email;
+
+            this.$scope.token = token;
+            this.$scope.emailAddress = email;
+
+            if (token && token.length)
+                this.$scope.model.code = token;
+
+            if (email && email.length)
+                this.$scope.model.email = email;
         }
 
         checkMatch() {
