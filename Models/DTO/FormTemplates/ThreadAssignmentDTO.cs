3d1cd56789164948529edbce808cfabdcae66e22
Armin Zia <arminzia@live.com>
2018-02-24 17:52:23 +0330

    Add DTOs for thread assignments, organisation teams and OrgTeamUsers.

---
+++ Models/DTO/FormTemplates/ThreadAssignmentDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LightMethods.Survey.Models.DTO
+{
+    public class ThreadAssignmentDTO
+    {
+        public Guid FormTemplateId { set; get; }
+
+        public Guid OrgUserId { set; get; }
+
+        public string OrgUserName { set; get; }
+
+        public bool IsRootUser { get; set; }
+
+        public bool? CanView { get; set; }
+
+        public bool? CanAdd { get; set; }
+
+        public bool? CanEdit { get; set; }
+
+        public bool? CanDelete { get; set; }
+    }
+}
---
+++ Models/DTO/Users/OrganisationTeamDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LightMethods.Survey.Models.DTO
+{
+    public class OrganisationTeamDTO
+    {
+        public string Id { get; set; }
+
+        public OrganisationDTO Organisation { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Colour { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public ICollection<OrgTeamUserDTO> Users { get; set; }
+    }
+}
